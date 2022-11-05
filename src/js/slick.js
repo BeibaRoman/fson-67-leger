05c49426ccdbd824590f83e211749527d936b836
Nikolay Mosalov <73695807+Rom-dev-tech@users.noreply.github.com>
2022-11-05 12:44:53 +0200

    add js and fix team

---
+++ src/js/slick.js
@@ -0,0 +1,11 @@
+$('.slick-js').slick({
+  dots: true,
+  arrows: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true,
+  autoplay: true,
+  autoplaySpeed: 5000,
+  // slidesToShow: 6,
+});
